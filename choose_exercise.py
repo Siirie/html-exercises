6cb326e5e9f305fdf99f3a62e8d4ebfe3fb699c9
Siiri <siiri.e.virtanen@gmail.com>
2025-06-05 15:24:53 +0300

    sama kuin edellinen?

---
+++ choose_exercise.py
@@ -0,0 +1,13 @@
+from random import sample
+#luokat 0-4, lkm 1-30. Tiedot tulee html formista
+#data josta valitaan on /salat/rally_kyltit.csv
+
+def choose_exe(luokat: tuple, lkm: int):
+    pass
+
+
+if __name__ == "__main__":
+    # Test the function with example inputs
+    luokat = (0, 1)
+    lkm = 5
+    print(choose_exe(luokat, lkm))  
---
+++ salat/rally_kyttien muotoilu.py
@@ -0,0 +1,16 @@
+
+""""
+#kirjoita ohjelma, joka lisää jokaisen rivin alkuun järjestysnumeron alkaen 1
+with open('salat/rally_kyltit_kopio.txt', 'r') as file:
+    lines = file.readlines()
+with open('salat/rally_kyltit_kopio.txt', 'w') as file:
+    for i, line in enumerate(lines, start=1):
+        file.write(f"{i};{line}")  # Add the line number at the beginning of each line
+"""
+
+with open('salat/rally_kyltit_kopio.txt', 'r') as file:
+    for row in file:
+        # Process each row in the file
+        parts= row.split(';')
+        print(parts[0], parts[1], parts[2], parts[3], parts[4]) 
+        #0 = kyltin järjestysnro, #1 = kyltin nro 2 nimi 3 suorituspaikkau 4 kuvaus
