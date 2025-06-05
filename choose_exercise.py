4b61496b8b6c97bfe6ba31b811e8330162f58157
Siiri <siiri.e.virtanen@gmail.com>
2025-06-05 17:34:22 +0300

    choosen exe toimii!

--- choose_exercise.py
+++ choose_exercise.py
@@ -4,24 +4,39 @@ from random import sample
 def get_pool(luokat: tuple):
     #luokat numero vastaa rally_kyltit_kopio.txt tiedoston jokaisen rivin alussa olevia numeroita
     pool = []
-    for luokka in luokat:
-        if luokka == 0:
-            pool.extend(range(2, 4))  # 2-3
-        elif luokka == 1:
-            pool.extend(range(4, 40))  # 4-39
-        elif luokka == 2:
-            pool.extend(range(40, 65))  # 40-64
-        elif luokka == 3:
-            pool.extend(range(65, 91))  # 65-90
-        elif luokka == 4:
-            pool.extend(range(91, 113))  # 91-112
+    try:
+        for luokka in range(luokat):
+            if luokka == 0:
+                pool.extend(range(2, 4))  # 2-3
+            elif luokka == 1:
+                pool.extend(range(4, 40))  # 4-39
+            elif luokka == 2:
+                pool.extend(range(40, 65))  # 40-64
+            elif luokka == 3:
+                pool.extend(range(65, 91))  # 65-90
+            elif luokka == 4:
+                pool.extend(range(91, 113))  # 91-112
+    except:
+        for luokka in luokat:
+            if luokka == 0:
+                pool.extend(range(2, 4))  # 2-3
+            elif luokka == 1:
+                pool.extend(range(4, 40))  # 4-39
+            elif luokka == 2:
+                pool.extend(range(40, 65))  # 40-64
+            elif luokka == 3:
+                pool.extend(range(65, 91))  # 65-90
+            elif luokka == 4:
+                pool.extend(range(91, 113))  # 91-112
+    else:
+        print("erroria pukkaa")
     return pool
 
-def choose_exe(luokat: tuple, lkm: int):
+def choose_exe(luokat: tuple, lkm: int): #tulostaa tuloksen (nro, nimi)
     pool= get_pool(luokat)
     possibilities =[]
     #luokat numero vastaa rally_kyltit_kopio.txt tiedoston jokaisen rivin alussa olevia numeroita
-    with open('salat/rally_kyltit_kopio.txt', 'r') as kyltitfile:
+    with open('salat/rally_kyltit_kopio.txt', 'r', encoding='utf-8') as kyltitfile:
         for row in kyltitfile:
             parts= row.split(';') #parts[0] on järjestysnumero
             #print(parts[0], parts[1], parts[2], parts[3], parts[4])
@@ -29,8 +44,17 @@ def choose_exe(luokat: tuple, lkm: int):
             if int(parts[0]) in pool:
                 possibilities.append(row.strip())
     
-    return sample(possibilities, lkm) if len(possibilities) >= lkm else possibilities
+    
+    choosen_exes=[]
+    if len(possibilities) >= lkm:
+        choosen_exes= sample(possibilities, lkm)
+    else:
+        choosen_exes=possibilities
+
+    for exes in choosen_exes:
         
+        parts= exes.split(';') #parts[0] on järjestysnumero
+        print(parts[1], parts[2])    
     #0 = 2, 3
     # 1 = 4-39
     #2 = 40-64
@@ -42,5 +66,8 @@ if __name__ == "__main__":
     # Test the function with example inputs
     #luokat = (0, 1)
     lkm = 5
-    print(choose_exe((0, 1), 3))  
-    print(choose_exe((0, 1), 1))      
+ #   print(choose_exe((0, 1), 3))  
+    #choose_exe((0, 1), 1) #Kutsutaan siis vain choose_exe(luokat, lkm) 
+    choose_exe((4, 1, 2), 10) 
+
+    
